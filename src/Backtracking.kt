6d1c031d4551e7584bde03224a616fc77143531e
Cerve <cerve.v55@gmail.com>
2020-03-07 12:01:00 -0500

    -Backtracking code clean up

--- src/Backtracking.kt
+++ src/Backtracking.kt
@@ -2,13 +2,17 @@ import java.util.stream.IntStream.range
 
 fun main(args: Array<String>){
 
-    board1().forEach(::println)
-    solve1(board1())
+    println("")
+    println("Starting Board")
+    board4().forEach(::println)
+    println("")
+    println("Solved Board")
+    solve1(board4())
 
     //println(findEmpty(board2()))
 
 }
-
+//Beginner
 fun board1():List<MutableList<Int>> {
 
     val list1 = mutableListOf(0,0,9,0,0,2,0,0,5)
@@ -24,36 +28,30 @@ fun board1():List<MutableList<Int>> {
 
     return listOf(list1,list2,list3,list4,list5,list6,list7,list8,list9)
 }
-fun board2():List<MutableList<Int>> {
 
-    val list1 = mutableListOf(4,7,9,1,3,2,6,8,5)
-    val list2 = mutableListOf(5,3,8,7,6,4,2,1,9)
-    val list3 = mutableListOf(1,6,2,0,0,0,0,3,0)
-    val list4 = mutableListOf(0,0,3,0,2,7,0,0,0)
-    val list5 = mutableListOf(0,5,4,6,0,0,1,0,0)
-    val list6 = mutableListOf(0,0,7,0,1,5,3,4,0)
-    val list7 = mutableListOf(3,0,0,8,0,1,9,0,6)
-    val list8 = mutableListOf(7,0,0,3,0,0,8,5,0)
-    val list9 = mutableListOf(0,9,1,0,0,0,4,7,0)
+//Expert
+fun board4():List<MutableList<Int>> {
+
+    val list1 = mutableListOf(3,0,0,1,0,0,0,8,0)
+    val list2 = mutableListOf(0,0,0,0,6,0,0,0,4)
+    val list3 = mutableListOf(5,6,0,2,0,0,0,0,0)
+    val list4 = mutableListOf(0,3,0,0,0,8,1,0,2)
+    val list5 = mutableListOf(6,0,0,0,0,0,0,7,0)
+    val list6 = mutableListOf(0,0,0,0,0,4,5,0,0)
+    val list7 = mutableListOf(0,0,1,0,0,9,7,0,0)
+    val list8 = mutableListOf(0,0,0,0,0,0,0,9,0)
+    val list9 = mutableListOf(0,2,0,0,8,0,0,0,0)
 
 
     return listOf(list1,list2,list3,list4,list5,list6,list7,list8,list9)
 }
-fun board3():List<MutableList<Int>> {
-
-    val list1 = mutableListOf(0,0,9,0,0,2,0,0,5)
-    val list2 = mutableListOf(5,3,8,0,6,4,0,0,9)
-    val list3 = mutableListOf(1,6,2,0,0,0,0,3,0)
-
-    return listOf(list1,list2,list3)
-}
-
 
 fun solve1(board: List<MutableList<Int>>):Boolean {
     //board.forEach(::println)
-    println(board)
+    //
     val position = findEmpty(board)
     if (position.isEmpty()) {
+        board.forEach(::println)
         return true}
     else{
         for (i in 1..9) {

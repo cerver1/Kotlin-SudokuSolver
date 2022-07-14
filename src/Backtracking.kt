fde81e456b6edb039d50ed2c29a597c3f0546496
cerver1 <cerve.v55@gmail.com>
2022-07-14 18:22:48 -0400

    minor clean_up

--- src/Backtracking.kt
+++ src/Backtracking.kt
@@ -1,103 +1,114 @@
 import java.util.stream.IntStream.range
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>){
 
-    println("")
-    println("Starting Board")
-    board4().forEach(::println)
-    println("")
-    println("Solved Board")
-    solve1(board4())
+    println("\nStarting Board\n")
+    (zeroedBoard()).map { it.contentToString() }.forEach(::println)
 
-    //println(findEmpty(board2()))
+    val time = measureTimeMillis {
+
+        println("\nSolved Board\n")
+        findSolution(zeroedBoard()).map { it.contentToString() }.forEach(::println)
+
+    }
+
+    println("\nsolved in $time ms\n")
 
 }
+
+fun zeroedBoard() = (1..9).map {
+    (1..9).map { 0 }.toTypedArray()
+}.toTypedArray()
+
 //Beginner
-fun board1():List<MutableList<Int>> {
+fun beginnerSudokuBoard() = arrayOf(
 
-    val list1 = mutableListOf(0,0,9,0,0,2,0,0,5)
-    val list2 = mutableListOf(5,3,8,0,6,4,0,0,9)
-    val list3 = mutableListOf(1,6,2,0,0,0,0,3,0)
-    val list4 = mutableListOf(0,0,3,0,2,7,0,0,0)
-    val list5 = mutableListOf(0,5,4,6,0,0,1,0,0)
-    val list6 = mutableListOf(0,0,7,0,1,5,3,4,0)
-    val list7 = mutableListOf(3,0,0,8,0,1,9,0,6)
-    val list8 = mutableListOf(7,0,0,3,0,0,8,5,0)
-    val list9 = mutableListOf(0,9,1,0,0,0,4,7,0)
+    arrayOf(0,0,9,0,0,2,0,0,5),
+    arrayOf(5,3,8,0,6,4,0,0,9),
+    arrayOf(1,6,2,0,0,0,0,3,0),
+    arrayOf(0,0,3,0,2,7,0,0,0),
+    arrayOf(0,5,4,6,0,0,1,0,0),
+    arrayOf(0,0,7,0,1,5,3,4,0),
+    arrayOf(3,0,0,8,0,1,9,0,6),
+    arrayOf(7,0,0,3,0,0,8,5,0),
+    arrayOf(0,9,1,0,0,0,4,7,0)
 
-
-    return listOf(list1,list2,list3,list4,list5,list6,list7,list8,list9)
-}
+)
 
 //Expert
-fun board4():List<MutableList<Int>> {
+fun expertSudokuBoard() = arrayOf(
 
-    val list1 = mutableListOf(3,0,0,1,0,0,0,8,0)
-    val list2 = mutableListOf(0,0,0,0,6,0,0,0,4)
-    val list3 = mutableListOf(5,6,0,2,0,0,0,0,0)
-    val list4 = mutableListOf(0,3,0,0,0,8,1,0,2)
-    val list5 = mutableListOf(6,0,0,0,0,0,0,7,0)
-    val list6 = mutableListOf(0,0,0,0,0,4,5,0,0)
-    val list7 = mutableListOf(0,0,1,0,0,9,7,0,0)
-    val list8 = mutableListOf(0,0,0,0,0,0,0,9,0)
-    val list9 = mutableListOf(0,2,0,0,8,0,0,0,0)
+    arrayOf(3,0,0,1,0,0,0,8,0),
+    arrayOf(0,0,0,0,6,0,0,0,4),
+    arrayOf(5,6,0,2,0,0,0,0,0),
+    arrayOf(0,3,0,0,0,8,1,0,2),
+    arrayOf(6,0,0,0,0,0,0,7,0),
+    arrayOf(0,0,0,0,0,4,5,0,0),
+    arrayOf(0,0,1,0,0,9,7,0,0),
+    arrayOf(0,0,0,0,0,0,0,9,0),
+    arrayOf(0,2,0,0,8,0,0,0,0)
 
+)
 
-    return listOf(list1,list2,list3,list4,list5,list6,list7,list8,list9)
-}
+fun findSolution(board: Array<Array<Int>>) : Array<Array<Int>> {
 
-fun solve1(board: List<MutableList<Int>>):Boolean {
-    //board.forEach(::println)
-    //
-    val position = findEmpty(board)
+    val position = findEmptyPosition(board)
+
     if (position.isEmpty()) {
-        board.forEach(::println)
-        return true}
-    else{
-        for (i in 1..9) {
-            if (validate1(board, i , position)){
-                board[position[0]][position[1]] = i
+        return board
+    } else {
 
-                if (solve1(board)){
-                    return true
-                }
+        (1..9).forEach { candidate ->
+
+            if (validatePlacement(board, candidate, position)) {
+
+                board[position[0]][position[1]] = candidate
+
+                if (findEmptyPosition(findSolution(board)).isEmpty()) return board
 
                 board[position[0]][position[1]] = 0
             }
+
         }
 
+        return board
+
     }
-    return  false
+
 }
-fun validate1(board: List<List<Int>>, number: Int, position: List<Int> ): Boolean{
-    //check row
-    if (board[position[0]].contains(number)) { return false }
-    //check column
+
+private fun validatePlacement(
+    board: Array<Array<Int>>,
+    number: Int,
+    position: List<Int>
+): Boolean {
+    // validate row
+    if (board[position[0]].contains(number)) return false
+
+    // validate column
     for (i in board) {
-        if (i[position[1]] == number){ return false} }
+        if (i[position[1]] == number) return false
+    }
 
-    val cordX = position[1] / 3
-    val cordY = position[0] / 3
+    val x = position[1] / 3
+    val y = position[0] / 3
 
-
-    for (i in range(cordY*3, (cordY*3)+ 3)){
-        for (j in range(cordX*3, (cordX*3)+3)){
-            if (board[i][j] == number && listOf(i,j) != position) {return false}
+    for (row in range(y*3, (y*3)+ 3)){
+        for (column in range(x*3, (x*3)+3)){
+            if (board[row][column] == number && listOf(row,column) != position) return false
         }
     }
 
     return true
 }
-fun findEmpty(board: List<List<Int>>): List<Int> {
 
-    for (i in board) {
-        for (j in i){
+private fun findEmptyPosition(board: Array<Array<Int>>): List<Int> {
 
-            //row = i ,column = j
-            if (j == 0) return listOf(board.indexOf(i), i.indexOf(j)) }
-
+    for (row in board) {
+        for (column in row) {
+            if (column == 0) return listOf(board.indexOf(row), row.indexOf(column))
+        }
     }
     return emptyList()
 }
-
-
